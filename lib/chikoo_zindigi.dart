7936bc952d573c09fff9c862f58038e3777fe6f4
lhan-brandverse-dev <ilhan@brandver.se>
2023-03-08 14:12:45 +0500

    revert

--- lib/chikoo_zindigi.dart
+++ lib/chikoo_zindigi.dart
@@ -1,23 +1,7 @@
 library chikoo_zindigi;
-import 'package:flutter/material.dart';
 
-
-class ChikooZindigi extends StatefulWidget {
-  const ChikooZindigi({super.key});
-
-  @override
-  State<ChikooZindigi> createState() => _ChikooZindigiState();
-}
-
-class _ChikooZindigiState extends State<ChikooZindigi> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: const [
-          Text('From chikoo zindigi package')
-        ],
-      ),
-    );
-  }
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
 }
